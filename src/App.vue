8d73f5a7ff85059bd12ff0731d9931c2f43bf207
Wayne Greeley <it199@hotmail.com>
2021-03-28 15:06:16 -0500

    adding pokeapi calls

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Wayne Greeley</h1>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Search PokeAPI</h1>
+
+      <div class="search-box">
+        <input 
+          type="text" 
+          placeholder="Search..."
+          v-model="query"
+          @keypress="fetchPokemon"
+          :class="[query.length < 1 || query.length > 3 ? 'red' : 'green']"
+        />
+      </div>
+
+      <div class="pokemon-wrap" v-if="typeof pokemon.name != 'undefined' ">
+        <div class="pokemon-box">
+          <div class="name">
+            {{ pokemon.name }}
+          </div>
+          <div class="type1">
+            {{ pokemon.types[0].type.name }}
+          </div>
+          <div class="type2" v-if="typeof pokemon.types[1] != 'undefined' ">
+            {{ pokemon.types[1].type.name}}
+          </div>
+          <div class="abilities" >
+            <ul>
+              <li v-for="(el, index) in pokemon.abilities" :key="index" >
+                {{ el.ability.name }}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+    <div>
+      <input v-model="email">
+      <button onclick="alert('sign up')" :disabled="email.length<4" >submit</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      url_base: 'https://pokeapi.co/api/v2/pokemon/',
+      query: '',
+      pokemon: {},
+      pokemondata: {},
+      abilies: [],
+      email: ''
+    }
+  },
+  methods: {
+    fetchPokemon (e) {
+      if (e.key == "Enter") {
+        fetch(`${this.url_base}${this.query}?j=extra`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      console.log(results);
+      this.pokemon = results;
+    },
+    async onFormSubmit() {
+      try {
+        this.pokemondata = await fetch(`${this.url_base}${this.query}?j=extra`);
+        // success
+      } catch (error) {
+        // error
+      }
+    }
   }
 }
 </script>
@@ -25,5 +86,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.red{
+  border: 2px solid red;
+}
+.green{
+  border: 2px solid green;
 }
 </style>
